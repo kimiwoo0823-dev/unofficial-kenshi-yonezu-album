98b935fab2c4aad617d27844d6ac0852f372519e
kimiwoo0823 <kimiwoo0823@gmail.com>
2026-06-05 12:05:26 +0900

    json file add

--- js/album.js
+++ js/album.js
@@ -5,10 +5,23 @@ fetch("/js/album.json") //파일 찾기
     const albumId = params.get("id"); //저장된 값에서 id 부분을 albumId에 저장
     const album = data.album.find((a) => a.id === albumId); //album의 값을 data(album.json)의 album 부분에 albumId 위치에 있는 값을 album에 저장
     const musictrack = document.querySelector(".musictrack"); //불러온 웹페이지에서 .musictrack
-    const cover = document.querySelector(".albumcover"); //cover 에 albumcover 클래스의 값을 불러오고
-    cover.src = album.image; //그 클래스가 있는 구성 요소에 src 부분에 album 위치의 image의 값을 불러옴
-    cover.alt = album.title; //그 클래스가 있는 구성 요소에 alt 부분에 album 위치의 title의 값을 불러옴
+    const albumvisual = document.querySelector(".albumvisual");
 
+    albumvisual.innerHTML = `
+      <div class="slider">
+          ${album.image
+            .map(
+              (img) => `
+              <img src="${img}" class="albumimage" alt="${album.title}">
+            `,
+            )
+            .join("")}
+        </div>
+      <div class="albumtitle">
+        <p class="albumnumber">${album.number}</p>
+        <p class="albumtext">${album.title}</p>
+      </div>
+        `;
     document.querySelector(".albumnumber").textContent = album.number;
     document.querySelector(".albumtext").textContent = album.title;
 
--- js/music.js
+++ js/music.js
@@ -3,16 +3,21 @@ fetch("/js/album.json")
   .then((data) => {
     const params = new URLSearchParams(location.search); //params에 url 값을 저장
     const musicId = params.get("id"); //저장된 값에서 id 부분을 musicId에 저장
-    const allmusic = [...data.album, ...data.single, ...data.digital];
-    const musiclist = allmusic.flatMap((item) => item.musics);
-    const music = musiclist.find((a) => a.id === musicId); //music의 값을 data(album.json)의 music 부분에 musicId 위치에 있는 값을 music에 저장
+    const music = data.musics.find((a) => a.id === musicId); //music의 값을 data(album.json)의 music 부분에 musicId 위치에 있는 값을 music에 저장
+    const currentAlbum = data.album.find((a) => a.id === music.albumId);
+    const currentSingle = data.single.find((a) => a.id === music.singleId);
     const title = document.querySelector(".musictitle");
     const mvis = document.querySelector(".mvis");
     const musicplay = document.querySelector(".musicplay");
     const lyrics = document.querySelector(".lyrics");
-    title.textContent = music.entitle;
-    console.log(music);
+    const toolalbum = document.querySelector(".toolalbum");
+    const toolsingle = document.querySelector(".toolsingle");
+    const lyricstool = document.querySelector(".lyricstool");
 
+    title.innerHTML = `${musicId}`;
+    console.log(music);
+    console.log(currentAlbum);
+    console.log(currentSingle);
     if (music.mv != "") {
       mvis.innerHTML = music.mv
         .map(
@@ -29,19 +34,72 @@ fetch("/js/album.json")
       .map(
         (_, mus) => `
         <div class="musicvisual">
-            <iframe src="${music.music[mus]}" class="musicvis"></iframe>
+            <iframe src="${music.music[mus]}" class="musicvis" allow></iframe>
         </div>
         `,
       )
       .join("");
-
-    lyrics.innerHTML = music.jplyrics
-      .map(
-        (_, lyr) => `
+    if (music.jplyrics != "") {
+      lyricstool.innerHTML = `
+      <div class="lyrics">
+      ${music.jplyrics
+        .map(
+          (_, lyr) => `
         <p class="lyricstext jplyrics">${music.jplyrics[lyr]}</p>
         <p class="lyricstext jpdlyrics">${music.jpdlyrics[lyr]}</p>
         <p class="lyricstext krlyrics">${music.krlyrics[lyr]}</p>
         `,
-      )
-      .join("");
+        )
+        .join("")}
+        </div>
+        `;
+    }
+    function musichtml(data, type) {
+      return `
+      <div class="${type}">
+        <div class="${type}title">
+        <a href="/${type}/${type}.html?id=${data.id}">
+          <img
+            src="/image/title/${type}/${data.id}.png"
+            class="titleimage"
+            alt="${data.id}"
+          />
+          </a>
+        </div>
+        <table>
+          <tr>
+            <td colspan="2">${data.date}</td>
+          </tr>
+          ${data.musics
+            .map(
+              (mus) => `
+          <tr class="${mus.id === musicId ? "bold" : ""}">
+            <td>${mus.track}</td>
+            <td><a href="/music.html?id=${mus.id}">${mus.jptitle}</a></td>
+          </tr>
+          `,
+            )
+            .join("")}
+        </table>
+      </div>
+      `;
+    }
+    if ((currentAlbum && !currentSingle) || (!currentAlbum && currentSingle)) {
+      document.querySelector(".tools").classList.add("single-mode");
+    }
+    if (currentAlbum) {
+      toolalbum.innerHTML = musichtml(currentAlbum, "album");
+    }
+    if (currentSingle) {
+      toolsingle.innerHTML = musichtml(currentSingle, "single");
+    }
+
+    const albumbackcolor = document.querySelector(".albumtitle");
+    const singlebackcolor = document.querySelector(".singletitle");
+    const albumbordercolor = document.querySelector(".album");
+    const singlebordercolor = document.querySelector(".single");
+    albumbackcolor.style.backgroundColor = currentAlbum.backcolor;
+    singlebackcolor.style.backgroundColor = currentSingle.backcolor;
+    albumbordercolor.style.borderColor = currentAlbum.bordercolor;
+    singlebordercolor.style.borderColor = currentSingle.bordercolor;
   });
