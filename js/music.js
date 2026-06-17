0e74718ac48986a292d5972b2f55fb230718e342
kimiwoo0823 <kimiwoo0823@gmail.com>
2026-06-17 13:57:30 +0900

    feat: lyrics fold, expand add

--- js/music.js
+++ js/music.js
@@ -42,9 +42,40 @@ fetch("/js/album.json")
       )
       .join("");
 
-    if (music.jplyrics != "") {
+    if (music.mvlyrics) {
       lyricstool.innerHTML = `
       <div class="lyricsbase">
+      <details>
+      <summary>MV 가사 보기</summary>
+      <table class="lyrics">
+      ${music.mvlyrics.jplyrics
+        .map(
+          (_, lyr) => `
+        <tr class="lyricstext">
+          <td></td>
+        </tr>
+        <tr class="lyricstext">
+          <td colspan=${!music.mvlyrics.jplyrics2[lyr] ? "2" : "1"}>${music.mvlyrics.jplyrics[lyr]}</td> <!-- 2번째 가사 없으면 2칸 차지 -->
+          <td>${music.mvlyrics.jplyrics2[lyr] ? music.mvlyrics.jplyrics2[lyr] : ""}</td>
+        </tr>
+        <tr class="lyricstext">
+          <td colspan=${!music.mvlyrics.jpdlyrics2[lyr] ? "2" : "1"}>${music.mvlyrics.jpdlyrics[lyr]}</td>
+          <td>${music.mvlyrics.jpdlyrics2[lyr] ? music.mvlyrics.jpdlyrics2[lyr] : ""}</td>
+        </tr>
+        <tr class="lyricstext">
+          <td colspan=${!music.mvlyrics.krlyrics2[lyr] ? "2" : "1"}>${music.mvlyrics.krlyrics[lyr]}</td>
+          <td>${music.mvlyrics.krlyrics2[lyr] ? music.mvlyrics.krlyrics2[lyr] : ""}</td>
+        </tr>
+        <tr class="lyricstext">
+          <td></td>
+        </tr>
+        `,
+        )
+        .join("")}
+        </table>
+        </details>
+      <details>
+      <summary>가사 보기</summary>
       <table class="lyrics">
       ${music.jplyrics
         .map(
@@ -71,6 +102,41 @@ fetch("/js/album.json")
         )
         .join("")}
         </table>
+        </details>
+        </div>
+        `;
+    } else if (music.jplyrics != "") {
+      lyricstool.innerHTML = `
+      <div class="lyricsbase">
+      <details>
+      <summary>가사 보기</summary>
+      <table class="lyrics">
+      ${music.jplyrics
+        .map(
+          (_, lyr) => `
+        <tr class="lyricstext">
+          <td></td>
+        </tr>
+        <tr class="lyricstext">
+          <td colspan=${!music.jplyrics2[lyr] ? "2" : "1"}>${music.jplyrics[lyr]}</td> <!-- 2번째 가사 없으면 2칸 차지 -->
+          <td>${music.jplyrics2[lyr] ? music.jplyrics2[lyr] : ""}</td>
+        </tr>
+        <tr class="lyricstext">
+          <td colspan=${!music.jpdlyrics2[lyr] ? "2" : "1"}>${music.jpdlyrics[lyr]}</td>
+          <td>${music.jpdlyrics2[lyr] ? music.jpdlyrics2[lyr] : ""}</td>
+        </tr>
+        <tr class="lyricstext">
+          <td colspan=${!music.krlyrics2[lyr] ? "2" : "1"}>${music.krlyrics[lyr]}</td>
+          <td>${music.krlyrics2[lyr] ? music.krlyrics2[lyr] : ""}</td>
+        </tr>
+        <tr class="lyricstext">
+          <td></td>
+        </tr>
+        `,
+        )
+        .join("")}
+        </table>
+        </details>
         </div>
         `;
     }
